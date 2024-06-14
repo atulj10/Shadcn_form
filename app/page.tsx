2d302bd254e723c4a70d23bfd0330c4c08e00b6a
atulj10 <atulanandj10@gmail.com>
2024-06-14 22:57:46 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-[100vh] flex flex-col justify-center items-center">
       <h1 className="text-6xl">HOME PAGE</h1>
-      <Link href={'/form'}><button className=" border-2 border-black py-2 px-4 rounded-lg my-8 hover:bg-black hover:text-white transition-all">Go to Login Page &nbsp; -></button></Link>
+      <Link href={'/form'}><button className=" border-2 border-black py-2 px-4 rounded-lg my-8 hover:bg-black hover:text-white transition-all">Go to Login Page &nbsp; {"->"}</button></Link>
     </div>
   );
 }
